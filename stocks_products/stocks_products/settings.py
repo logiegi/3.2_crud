6ed06a01900d5530b80b1f3c06c38237a1e778ce
Egor <les.med@mail.ru>
2024-10-12 16:06:22 +0300

    change

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'logistic',
-    # 'django_filters'
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -134,6 +134,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 1
 }
